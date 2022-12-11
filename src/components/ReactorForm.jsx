be560b6e864e1800d69e84b4491de52540b8170a
Joel Lopez Villarreal <joell@southern.edu>
2022-12-11 09:50:53 -0500

    Added snackbars

--- src/components/ReactorForm.jsx
+++ src/components/ReactorForm.jsx
@@ -6,12 +6,16 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import EditIcon from '@mui/icons-material/Edit';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom'
+import { useSnackbar } from "notistack"
 import { useState } from 'react';
 
 
 function ReactorForm({ reactorName, id }) {
     const [open, setOpen] = useState(false)
     const [name, setName] = useState("")
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar()
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -22,9 +26,9 @@ function ReactorForm({ reactorName, id }) {
     };
 
     const changeName = async () => {
-        console.log("fetching")
         try {
-            await fetch(`https://nuclear.dacoder.io/reactors/set-reactor-name/${id}?apiKey=6cc0a3fa7141b32d`, {
+            setIsSubmitting(true)
+            const response =  await fetch(`https://nuclear.dacoder.io/reactors/set-reactor-name/${id}?apiKey=6cc0a3fa7141b32d`, {
                 method: "PUT",
                 headers: {
                     'Accept': 'application/json',
@@ -34,10 +38,17 @@ function ReactorForm({ reactorName, id }) {
                     name
                 })
             })
-            console.log("succeed")
-            handleClose()
+
+            if (!response.ok) {
+                const errorMessage = await response.json()
+                enqueueSnackbar(errorMessage.message, {
+                    preventDuplicate: true
+                })
+            }
         } catch (error) {
-            console.log(error)
+        } finally {
+            setIsSubmitting(false)
+            handleClose()
         }
     }
 
@@ -73,7 +84,7 @@ function ReactorForm({ reactorName, id }) {
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleClose}>Cancel</Button>
-                        <Button onClick={changeName}>Submit</Button>
+                        <Button onClick={changeName}>{isSubmitting ? <HourglassBottomIcon /> : "Submit"}</Button>
                     </DialogActions>
                 </Dialog>
             </div>
